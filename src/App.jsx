f00fffc15925475f7daf402e97a667bdce0bafc4
ErnestoCisnero <miguelernestocisnero@gmail.com>
2022-08-09 21:38:00 -0400

    Add redux, react-router and MUI configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Box, Container, Typography } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Trakerly</h1>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Trakerly
+        </Typography>
+      </Box>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './appStore/store';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import theme from './theme/theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import './index.css';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -11,7 +14,12 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={ theme }>
+        <CssBaseline />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
