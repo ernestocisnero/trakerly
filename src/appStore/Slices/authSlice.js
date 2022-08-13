ca7b00f3126d1a3fed061d64a6293208790918f9
ErnestoCisnero <miguelernestocisnero@gmail.com>
2022-08-12 21:53:00 -0400

    add actions generators AuthSlice

--- src/appStore/Slices/authSlice.js
+++ src/appStore/Slices/authSlice.js
@@ -10,14 +10,25 @@ const initialState = {
 }
 
 export const authSlice = createSlice({
-    name: 'auht',
+    name: 'auth',
     initialState,
+
     reducers: {
         login: ( state, { payload } )=>{
-
+            state.status = 'authenticated';
+            state.displayName = payload.displayName;
+            state.email = payload.email;
+            state.uid = payload.uid;
+            state.photoURL = payload.photoURL;
+            state.errorMessage = payload.errorMessage;
         },
         logout: ( state, { payload } )=>{
-
+            state.status = 'not-authenticated';
+            state.displayName = null;
+            state.email = null;
+            state.uid = null;
+            state.photoURL = null;
+            state.errorMessage = payload.errorMessage;
         },
         authenticating: ( state )=>{
             state.status = 'checking';
