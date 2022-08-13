4d153cc9b0eceb316988dd3ea663ed334114af93
ErnestoCisnero <miguelernestocisnero@gmail.com>
2022-08-13 09:26:14 -0400

    add firebase auth configs

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDobvtxUr4KsRViIhqerhHVt9Jzaw6c2DQ",
+    authDomain: "trakerly.firebaseapp.com",
+    projectId: "trakerly",
+    storageBucket: "trakerly.appspot.com",
+    messagingSenderId: "558010897800",
+    appId: "1:558010897800:web:e17a6ad08a186c1e2cd63d"
+};
+
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth( firebaseApp );
