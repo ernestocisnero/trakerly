b077374be75db9a12849a3ecdb360bbd55ba66b4
ErnestoCisnero <miguelernestocisnero@gmail.com>
2022-08-29 11:44:36 -0400

    add incomes page UI

--- src/appStore/Thunks/inputsThunks.js
+++ src/appStore/Thunks/inputsThunks.js
@@ -3,20 +3,20 @@ import { firebaseDB } from "../../firebase/config";
 
 
 export const addIncome = () => {
-    // return async (dispatch, getState) => {
-    //     const userID = getState().auth.uid;
+    return async (dispatch, getState) => {
+        const userID = getState().auth.uid;
         
 
-    //     try {
-    //         const docRef = await addDoc(collection(firebaseDB, `${userID}/input/incomes`), {
-    //             income: 1500,
-    //             category: "Salary",
-    //             date: new Date().getTime() 
-    //         });
-    //         console.log("Document written with ID: ", docRef.id);
-    //     } catch (e) {
-    //         console.error("Error adding document: ", e);
-    //     }
+        try {
+            const docRef = await addDoc(collection(firebaseDB, `${userID}/input/incomes`), {
+                income: 1500,
+                category: "Salary",
+                date: new Date().getTime() 
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
 
-    // }
+    }
 }
--- src/trakerly/pages/AddIncome.jsx
+++ src/trakerly/pages/AddIncome.jsx
@@ -1,25 +1,100 @@
 import { useDispatch } from 'react-redux';
-import { Button } from "@mui/material"
+import { Box, Button, MenuItem, TextField, Typography } from "@mui/material"
 import { addIncome } from "../../appStore/Thunks/inputsThunks"
 import { TrakerlyLayout } from "../layout/TrakerlyLayout"
+import { useState } from 'react';
+
+
+const categories = [
+  {
+    value: 'Salary'
+  },
+  {
+    value: 'Gift'
+  },
+  {
+    value: 'Investment'
+  },
+  {
+    value: 'O ther'
+  },
+];
+
 
 export const AddIncome = () => {
 
   const dispatch = useDispatch();
 
-  const handleClick = ()=>{
-    dispatch( addIncome() );
+  const [category, setCategpry] = useState('');
+
+  const handleChangeCategory = (event) => {
+    setCategpry(event.target.value);
+  };
+
+  const handleClick = () => {
+    //dispatch( addIncome() );
   }
 
   return (
     <TrakerlyLayout>
-        <Button 
-                        variant="contained" 
-                        sx={{ color:'#8D8CC0', mx:8, backgroundColor:'#D9D9D9', ":hover":{ backgroundColor:'#D9D9D9' } }}
-                        onClick={ handleClick }
-                    >
-                        Register new income
-                    </Button>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'space-around' }}>
+        <Typography
+          variant='h5'
+          component='h1'
+          sx={{ color: 'primary.main', mb: 3 }}> Enter new income
+        </Typography>
+
+        <Box
+          component="form"
+          sx={{
+            mb:3,
+            display: 'flex', 
+            flexDirection: 'column', 
+            alignItems: 'space-around',
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+            <TextField
+              required
+              id="outlined-required"
+              label="income amount"
+              type='number'
+              helperText="Please select one category"
+            />
+
+            <TextField
+              required
+              id="outlined-select-currency"
+              select
+              label="Select"
+              value={category}
+              onChange={handleChangeCategory}
+              helperText="Please select one category"
+            >
+              {categories.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.value}
+                </MenuItem>
+              ))}
+            </TextField>
+        </Box>
+
+        <Button
+          variant="contained"
+          sx={{
+            width: '20%',
+            color: '#8D8CC0',
+            mx: 8,
+            backgroundColor: '#D9D9D9',
+            ":hover": { backgroundColor: '#D9D9D9' }
+          }}
+          onClick={handleClick}
+        >Done
+        </Button>
+      </Box>
+
     </TrakerlyLayout>
   )
 }
